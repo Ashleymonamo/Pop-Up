319991387a5f80b4678691c74f20d3ae060f15c1
Nape Ashley Monamo <107757882+Ashleymonamo@users.noreply.github.com>
2023-10-07 13:54:06 -0700

    Fully functioning pop up

---
+++ script.js
@@ -0,0 +1,9 @@
+let popup=document.getElementById("popup");
+//Open the pop up
+function openPopUp() {
+    popup.classList.add("open-popup");
+}
+//close the pop up
+function closePopUp() {
+    popup.classList.remove("open-popup");
+}
